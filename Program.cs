499433316730816bc17a683b44fca11e48ec79a9
MHY Express <mcyalda@yahoo.com>
2018-04-29 17:28:16 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Roll And Dice Game");
+            Console.WriteLine("please enter or press Enter to begin the Game ?");
+            string entrance = Console.ReadLine();
+
+            while (string.IsNullOrEmpty(entrance))
+            {
+                Console.WriteLine("roll the dice (y/n)");
+                string input = Console.ReadLine();
+
+                if (input == "y")
+                {
+                    Random up = new Random();
+                  
+
+                    int x = up.Next(1, 6);
+                    int y = up.Next(1, 6);
+
+                    Console.WriteLine($"the number are {x} {y}");
+                }
+                break;
+            }      
+        }
+    }
+}
